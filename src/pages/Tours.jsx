c35b6e9b24162856b23335cce04fdb4feb312f3e
Raktim Maity <108602390+Raktimmaity@users.noreply.github.com>
2025-06-02 15:13:37 +0530

    Update Tours.jsx

--- src/pages/Tours.jsx
+++ src/pages/Tours.jsx
@@ -6,7 +6,7 @@ import parisImage from "../assets/img/paris.jpg";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function Tours() {
-  const URL = "http://localhost:5000";
+  const URL = "ttps://tourist-backend-5qoo.onrender.com";
   const [filterCity, setFilterCity] = useState("");
   const [dateFilter, setDateFilter] = useState("all");
   const [selectedDate, setSelectedDate] = useState("");
